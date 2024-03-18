0afe07ad07d1901d3b2715fd96da32dcc35b1a6c
Swati Subhash Rathod <rathodswati193@gmail.com>
2024-03-18 11:00:36 +0530

    cityManagmentproject commited

--- CityManagementProject/src/main/java/com/example/management/repository/ComplaintRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.management.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.management.model.Complaint;
-
-public interface ComplaintRepository extends JpaRepository<Complaint, Integer>{
-
-}
